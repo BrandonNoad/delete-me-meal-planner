a2af19371067c3c4b732e5e52c6527ca08960a3d
Brandon Noad <Brandon.Noad@gmail.com>
2018-07-23 22:04:45 -0400

    Added grommet

---
+++ client/src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Article, Header, Section, Footer } from 'grommet';
+
+const App = () => (
+    <Article>
+        <Header></Header>
+            <Section>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio rerum, esse dolore atque vero molestiae accusantium ea repudiandae maxime iure nostrum voluptates iusto suscipit aliquid voluptas sunt voluptate! Repellendus, laboriosam!</Section>
+        <Footer></Footer>
+    </Article>
+);
+
+export default App;
