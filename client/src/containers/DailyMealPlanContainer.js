6b1c2b6926a7d7a31b40f6e653fe9f7b7836c8d4
Brandon Noad <Brandon.Noad@gmail.com>
2019-02-17 08:24:16 -0500

    Use useEffect hook to fetch data

--- client/src/containers/DailyMealPlanContainer.js
+++ client/src/containers/DailyMealPlanContainer.js
@@ -1,62 +1,43 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { fetchScheduledRecipesForDay, openAddRecipesModal } from '../actions';
 import { getScheduledRecipesForDay, getMetaForDay } from '../reducers/scheduledRecipes';
 import DailyMealPlan from '../components/DailyMealPlan';
 
-class DailyMealPlanContainer extends Component {
+const DailyMealPlanContainer = (props) => {
+    const { moment, scheduledRecipes, meta, fetchScheduledRecipes } = props;
 
-    fetchData() {
-
-        const { moment, fetchScheduledRecipes } = this.props;
-
+    useEffect(() => {
         fetchScheduledRecipes(moment);
+    }, [moment]);
+
+    // TODO: move the loading indicator inside the tile
+    if (
+        meta !== undefined &&
+        meta.isFetching &&
+        (scheduledRecipes === undefined || !scheduledRecipes.length)
+    ) {
+        return <p>Loading...</p>;
     }
 
-    componentDidMount() {
-
-        this.fetchData();
+    // TODO: move the error msg inside the tile
+    if (
+        meta !== undefined &&
+        meta.errorMessage &&
+        (scheduledRecipes === undefined || !scheduledRecipes.length)
+    ) {
+        return <p>{meta.errorMessage}</p>;
     }
 
-    componentDidUpdate(prevProps) {
+    return <DailyMealPlan {...props} />;
+};
 
-        this.fetchData();
-    }
+const mapStateToProps = (state, ownProps) => ({
+    scheduledRecipes: getScheduledRecipesForDay(state.scheduledRecipes, ownProps.moment),
+    meta: getMetaForDay(state.scheduledRecipes, ownProps.moment)
+});
 
-    render() {
-
-        const { meta, scheduledRecipes } = this.props;
-
-        // TODO: move the loading indicator inside the tile
-        if (meta !== undefined &&
-            meta.isFetching &&
-            (scheduledRecipes === undefined || !scheduledRecipes.length)) {
-
-            return <p>Loading...</p>;
-        }
-
-        // TODO: move the error msg inside the tile
-        if (meta !== undefined &&
-            meta.errorMessage &&
-            (scheduledRecipes === undefined || !scheduledRecipes.length)) {
-
-            return <p>{meta.errorMessage}</p>;
-        }
-
-        return <DailyMealPlan  {...this.props} />;
-    }
-}
-
-const mapStateToProps = (state, ownProps) => (
-    {
-        scheduledRecipes: getScheduledRecipesForDay(state.scheduledRecipes, ownProps.moment),
-        meta: getMetaForDay(state.scheduledRecipes, ownProps.moment)
-    }
-);
-
-DailyMealPlanContainer = connect(
+export default connect(
     mapStateToProps,
     { fetchScheduledRecipes: fetchScheduledRecipesForDay, openAddRecipesModal }
 )(DailyMealPlanContainer);
-
-export default DailyMealPlanContainer;
